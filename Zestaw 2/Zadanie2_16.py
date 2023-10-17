0259523e0937514b9ac96f9d25594194d0fdc557
Jan Kaleta <kaletajan57@gmail.com>
2023-10-17 14:08:10 +0200

    Zadanie 2.16

--- Zestaw 2/Zadanie2_16.py
+++ Zestaw 2/Zadanie2_16.py
@@ -2,3 +2,14 @@
 ZADANIE 2.16
 W tekście znajdującym się w zmiennej line zamienić ciąg znaków "GvR" na "Guido van Rossum".
 """
+
+def replace_str(string: str, to_replace: str, replacement: str) -> str:
+    return string.replace(to_replace, replacement)
+
+def main() -> None:
+    string = "W tym GvR zdaniu, podmieniamy GvR na inny GvR napis, który GvR zastąpi GvR"
+    print(f'Przed zamianą: {string}')
+    print(f'Po zamianie: {replace_str(string, "GvR", "Guido van Rossum")}')
+
+if __name__ == "__main__":
+    main()
