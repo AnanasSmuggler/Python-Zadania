99ea99570763fa70b27f9113fffd0148b971667c
Jan Kaleta <kaletajan57@gmail.com>
2023-10-17 14:36:18 +0200

    Zadanie 2.18

--- Zestaw 2/Zadanie2_18.py
+++ Zestaw 2/Zadanie2_18.py
@@ -2,3 +2,15 @@
 ZADANIE 2.18
 Znaleźć liczbę cyfr zero w dużej liczbie całkowitej. Wskazówka: zamienić liczbę na napis.
 """
+import re
+
+def count_zeros_in_int(integer: int) -> int:
+    string = str(integer)
+    return len(re.sub(r'[1-9]', '', string)) #wyrażenie regularne zamienia wszystkie cyfry różne od 0 na ''
+
+def main() -> None:
+    integer = 10928099002920
+    print(f'Liczba zer w liczbie całkowitej {integer}: {count_zeros_in_int(integer)}')
+
+if __name__ == "__main__":
+    main()
