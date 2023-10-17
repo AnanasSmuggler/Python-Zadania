b6caa9e91e6370ed1c3f6a6c48f465e5db99d60b
Jan Kaleta <kaletajan57@gmail.com>
2023-10-17 13:02:26 +0200

    Zadanie 2.15

--- Zestaw 2/Zadanie2_15.py
+++ Zestaw 2/Zadanie2_15.py
@@ -2,3 +2,15 @@
 ZADANIE 2.15
 Na liście L znajdują się liczby całkowite dodatnie. Stworzyć napis będący ciągiem cyfr kolejnych liczb z listy L.
 """
+
+def create_string_from_int_list(l: list) -> str:
+    str_list = [str(i) for i in l]
+    
+    return "".join(str_list)
+
+def main() -> None:
+    num_list = [7,12,34,19,63,5,28,42,9,51]
+    print(create_string_from_int_list(num_list))
+
+if __name__ == "__main__":
+    main()
