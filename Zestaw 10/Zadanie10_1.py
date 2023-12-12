f5aab944df2ea36fa15574a27d4efad25a13ebfd
Jan Kaleta <kaletajan57@gmail.com>
2023-12-12 13:16:10 +0100

    Zestaw 10 - poprawka

--- Zestaw 10/Zadanie10_1.py
+++ Zestaw 10/Zadanie10_1.py
@@ -1,3 +1,12 @@
+"""
+ZADANIE 10.1
+Napisać program z GUI, który symuluje rzut kostką sześcienną. Program 
+powinien mieć przycisk uruchamiający rzut kostką i etykietę wyświetlającą 
+wynik (liczba od 1 do 6 lub odpowiedni obrazek).
+
+Autor: Jan Kaleta
+"""
+
 import tkinter as tk
 import random
 
